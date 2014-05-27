c55fa03f44f5754b58c46396e9d0fce91a2bc2a6
Brandon <asydix@gmail.com>
2014-05-27 17:25:26 -0300

    Registering command

--- src/main/java/com/caved_in/stackeditems/StackedItems.java
+++ src/main/java/com/caved_in/stackeditems/StackedItems.java
@@ -1,6 +1,8 @@
 package com.caved_in.stackeditems;
 
+import com.caved_in.commons.command.CommandController;
 import com.caved_in.commons.plugin.Plugins;
+import com.caved_in.stackeditems.commands.StackCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StackedItems extends JavaPlugin {
@@ -14,6 +16,7 @@ public class StackedItems extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
+		CommandController.registerCommands(this,new StackCommand());
 		saveConfiguration();
 		Plugins.unregisterHooks(this);
 	}
